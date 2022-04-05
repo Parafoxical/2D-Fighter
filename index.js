f1094d36037a7c02b701a31f80777ad423fd34d8
Parafoxical <colejbarrett98@gmail.com>
2022-04-05 14:42:46 +0100

    V 1.1
    + Tweaked UI
    + Added Custom Font

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ function determineWinner({player,enemy,timerId}) {
     clearTimeout(timerId)
     document.querySelector('#displayText').style.display = 'flex'
     if (player.health === enemy.health) {
-        document.querySelector('#displayText').innerHTML = 'Tie'
+        document.querySelector('#displayText').innerHTML = 'Tie!'
     } else if (player.health > enemy.health) {
         document.querySelector('#displayText').innerHTML = 'Player 1 Wins!'
     } else if (player.health < enemy.health) {
